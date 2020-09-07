6c3c323920022cedb046643ffd734e094c49c326
Your Name <you@example.com>
2020-09-07 22:04:47 +0300

    Updates

---
+++ precache-manifest.1ef213c0bcaadfe2c6d2052a7343bbc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e4c322eb12e7cffa8ea649366ba78b",
+    "url": "/React-JSON-URL/index.html"
+  },
+  {
+    "revision": "82b532c5e2be14c5a669",
+    "url": "/React-JSON-URL/static/css/main.6d9e4845.chunk.css"
+  },
+  {
+    "revision": "1390d32039d4a8fbb2fb",
+    "url": "/React-JSON-URL/static/js/2.a848275e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-JSON-URL/static/js/2.a848275e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b532c5e2be14c5a669",
+    "url": "/React-JSON-URL/static/js/main.d02095fb.chunk.js"
+  },
+  {
+    "revision": "bf8338c1c5fdd9e1a75b",
+    "url": "/React-JSON-URL/static/js/runtime-main.0429cb20.js"
+  }
+]);
